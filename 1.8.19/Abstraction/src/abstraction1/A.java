e00218b26ffaeb815497723b32dd30e9841d9f58
assu <ashuvanwe21@gmail.com>
2020-12-22 20:18:10 +0530

    initial commit

---
+++ 1.8.19/Abstraction/src/abstraction1/A.java
@@ -0,0 +1,17 @@
+package abstraction1;
+
+public abstract class A 
+{
+	 public void m1()
+	 {
+	  System.out.println("m1--A");
+	 }
+		 public void m2()
+		  {
+		  System.out.println("m2--A");
+		  }
+		 public abstract void  m3();
+		
+
+
+}
